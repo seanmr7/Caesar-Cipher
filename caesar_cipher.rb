dc90b006fab5e7a7dbeeb0b2c5df14fa2f67d57e
Sean Riedl <smriedl@gmail.com>
2020-09-10 21:15:21 -0400

    Add check for non-letter characters

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,17 +4,16 @@ def caesar_cipher(string, shift)
     letters = string.split("")
     shiftedString = []
     letters.each do |letter|
-        if letter == " "
-            newAscii = 32
-        else
         ascii = letter.ord
         newAscii = ascii + shift
-        end
         if newAscii >= 123
             newAscii = 97 + (newAscii - 123)
         end
         if newAscii >=91 && newAscii < 96
             newAscii = 65 + (newAscii - 91)
+        end
+        if ascii <= 64 || (ascii > 90 && ascii < 97) || ascii >= 123
+            newAscii = ascii
         end
         newLetter = newAscii.chr
         shiftedString.push(newLetter)
@@ -25,4 +24,4 @@ def caesar_cipher(string, shift)
 
 end
 
-caesar_cipher("MY name is Sean", 5)
+caesar_cipher("What a string!", 5)
